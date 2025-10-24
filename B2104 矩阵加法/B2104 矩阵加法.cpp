1b81e3c6b0c4bef7aa8c779ed8f19d04fa84c704
FuHaoEnZicha <fuhaoen_doudou2025@qq.com>
2025-10-25 07:07:05 +0800

    fuhaoen

---
+++ B2104 矩阵加法/B2104 矩阵加法.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[105][105],b[105][105],c[105][105];
+    int n,m;
+    cin >> n >> m;
+    for (int i = 1;i <= n;i++)
+        for (int j = 1;j <= m;j++)
+            cin >> a[i][j];
+    for (int i = 1;i <= n;i++)
+        for (int j = 1;j <= m;j++)
+            cin >> b[i][j];
+    for (int i = 1;i <= n;i++)
+        for (int j = 1;j <= m;j++)
+            c[i][j] = a[i][j] + b[i][j];
+    for (int i = 1;i <= n;i++){
+        for (int j = 1;j <= m;j++){
+            cout << c[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
