62dc4e3600977c124f99e2992c33dae0a54f4726
FuHaoEnZicha <fuhaoen_doudou2025@qq.com>
2025-10-25 17:53:58 +0800

    Create 代码框架.cpp

---
+++ 代码框架.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+namespace maths{ //数学命名空间
+    #define PI 3.1415926
+    long long max(long long a,long long b){
+        if(a>b){return a;}
+        return b;}
+    long long min(long long a,long long b){
+        if (a<b){return a;}
+        return b;}
+}
+using namespace maths;
+using namespace std;
+int main()        //主函数
+{
+
+    return 0;
+}
